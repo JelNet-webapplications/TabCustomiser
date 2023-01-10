230f47321356071190ef7c58785762434ae71645
JelCraft <Jelcraft@outlook.com>
2023-01-10 18:26:17 +0100

    Live Sessie
    Co-authored-by: vivaloz <Vivaloz@users.noreply.github.com>

--- TabCustomiser/scripts/content.js
+++ TabCustomiser/scripts/content.js
@@ -8,16 +8,75 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         document.title = request.title;
     }
 })
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    newicon_path(request.path)
+})
 
 //-- Rename using keybind --
 document.addEventListener("keydown", (event) => {
-    if(event.shiftKey && event.altKey && event.code == 'KeyR') {
-        console.log("Keybind pressed!")
-        let prompt = window.prompt('Rename Tab:')
-        if(prompt) {
-            document.title = prompt
-        } else {
-            document.title = originalTitle
+    if(event.shiftKey && event.altKey){
+        if(event.code == 'KeyR'){
+            console.log("Keybind pressed!")
+            let prompt = window.prompt('Rename Tab:')
+            if(prompt) {
+                document.title = prompt
+            } else {
+                document.title = originalTitle
+            }
+        } else if(event.code == 'KeyF'){
+            newicon();
         }
     }
 })
+
+// chrome.runtime.onMessage.addListener(
+//     function(request, sender, sendResponse) {
+//       console.log(sender.tab ?
+//                   "from a content script:" + sender.tab.url :
+//                   "from the extension");
+//       if (request.greeting === "hello")
+//         sendResponse({farewell: "goodbye"});
+//     }
+//   );
+
+
+function newicon(){
+    let picture = window.prompt("picture URL"),
+        urlR = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi,
+        regexurl = new RegExp(urlR);
+    let alreadylinks = document.querySelectorAll("link[rel~='icon']");
+    alreadylinks.forEach(element =>{
+        element.remove();
+    });
+    let link = document.createElement('link');
+    link.rel = 'icon';
+    link.type = "image/png";
+    document.getElementsByTagName('head')[0].appendChild(link);
+    if(!picture.match(regexurl)){
+        return alert("incorrect url");
+    }
+    if(picture){
+        console.log(picture);
+        link.href = picture;
+        link.mce_href = picture;
+        // sommige sites hebben "mcehref" geen idee wat dat mag zijn maar het werkt in ieder geval niet.
+    }
+}
+
+function newicon_path(path){
+    let picture = path;
+    let alreadylinks = document.querySelectorAll("link[rel~='icon']");
+    alreadylinks.forEach(element =>{
+        element.remove();
+    });
+    let link = document.createElement('link');
+    link.rel = 'icon';
+    link.type = "image/png";
+    document.getElementsByTagName('head')[0].appendChild(link);
+    if(picture){
+        console.log(picture);
+        link.href = picture;
+        link.mce_href = picture;
+        // sommige sites hebben "mcehref" geen idee wat dat mag zijn maar het werkt in ieder geval niet.
+    }
+}
--- TabCustomiser/scripts/popup.js
+++ TabCustomiser/scripts/popup.js
@@ -7,6 +7,22 @@ function updateCursed() {
     }
 }
 
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.id === "getFaviconURL") {
+        sendResponse({URL: "test"}); 
+      } else {
+        sendResponse({ERR: "not found"});
+      }
+    }
+  );
+
+// let [ tab ] = await chrome.tabs.query({active:true});
+//     return tab.favIconUrl;
+
+
+
+
 //-- Rename using popup --
 let titleInput = document.querySelector('#tabName'); //Def input elem
 getCurrentTab().then(tab => titleInput.value = tab.title); //Update input value to match tab name
@@ -15,6 +31,21 @@ titleInput.addEventListener('keyup', () => {
     getCurrentTab().then(tab => chrome.tabs.sendMessage(tab.id, { title: titleInput.value }));
 
 });
+
+//-- Customise Icon using popup --
+let colorSelector = document.querySelector('#colorSelector');
+colorSelector.addEventListener('click', (event) => {
+    if(!event.target.id.includes('🎨')) return;
+    let path = "/colors/"+ event.target.id.substring(2,7) +".png"
+    
+    getCurrentTab().then(tab => chrome.tabs.sendMessage(tab.id, { title: titleInput.value }));
+})
+
+// -- Custom Favicon using popup
+let faviconApply = document.querySelector("#faviconapply")
+faviconApply.addEventListener("click", ()=>{
+      getCurrentTab().then(tab => chrome.tabs.sendMessage(tab.id, { path: document.querySelector('#faviconnew').value}))
+})
 
 //Utils
 async function getCurrentTab(){
